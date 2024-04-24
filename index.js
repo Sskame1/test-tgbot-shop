f52f462a45263f670e53e3ca4f89e0e516e1720e
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-04-24 15:55:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -68,6 +68,9 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
-const PORT = 8000;
+const PORT = 3000;
 
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+module.exports = app;
+
+// app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+
